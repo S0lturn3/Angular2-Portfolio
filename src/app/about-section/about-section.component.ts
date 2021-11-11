052628d1ef1093b8f876aa8c11f3f2bdffea0fc5
Erick Carvalho <erickzcarvalho25@outlook.com>
2021-11-10 23:38:37 -0300

    v1.2

--- src/app/about-section/about-section.component.ts
+++ src/app/about-section/about-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-section',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class AboutSectionComponent implements OnInit {
 
   constructor() { }
+
+  @Input() colorValue!: string;
 
   ngOnInit(): void {
   }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import * as EventEmitter from 'events';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular 2 Portfolio';
+
+  colorValue: string = "escuro";
+
+  ngOnInit() {
+    this.colorValue = 'escuro';
+  }
+
+  changeValue(){
+    switch(this.colorValue){
+      case 'escuro':
+        this.colorValue = "claro";
+        break;
+
+      case 'claro':
+        this.colorValue = "escuro";
+        break;
+    }
+  }
 }
